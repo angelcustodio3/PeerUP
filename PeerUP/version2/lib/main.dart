bf054f49b58b6f8e7ba1e1b1d573913f56cfd11f
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2023-11-02 12:23:53 +0800

    Editted main.dart file

--- PeerUP/version2/lib/main.dart
+++ PeerUP/version2/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:version2/homepage/main_page.dart';
+import 'package:peerup/homepage/main_page.dart';
 
 void main() {
   runApp(const MyApp());
