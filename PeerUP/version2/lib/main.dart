4961fecac31d4fc0d81b1dcf84808ee14e4ba9a1
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2023-11-07 03:14:13 +0800

    Editted app title

--- PeerUP/version2/lib/main.dart
+++ PeerUP/version2/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'PeerUP',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
