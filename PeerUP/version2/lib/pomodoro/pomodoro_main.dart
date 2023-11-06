ee3ff3943dd306033ec9bbfb269d89c771916296
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2023-11-06 14:14:36 +0800

    Updated Pomodoro page UI design

--- PeerUP/version2/lib/pomodoro/pomodoro_main.dart
+++ PeerUP/version2/lib/pomodoro/pomodoro_main.dart
@@ -19,6 +19,7 @@ class _PomodoroState extends State<Pomodoro> {
   Widget build(BuildContext context) {
     
     return Scaffold(
+      backgroundColor: const Color(0xFFF9F7F3),
       appBar: AppBar(
         title: const Text(
           'POMODORO',
@@ -29,10 +30,9 @@ class _PomodoroState extends State<Pomodoro> {
             fontWeight: FontWeight.bold
           ),
         ),
-        backgroundColor: const Color(0xFF6493A5),
-        elevation: 0.0,
+        backgroundColor: const Color(0xFF0FA3B1),
         centerTitle: true,
-        
+        elevation: 0.0,
         leading: GestureDetector(
           onTap: () {
             Navigator.push(
@@ -44,82 +44,91 @@ class _PomodoroState extends State<Pomodoro> {
             margin: const EdgeInsets.all(10),
             alignment: Alignment.center,
             decoration: const BoxDecoration(
-              color: Color(0xFF6493A5),
+              color: Color(0xFF0FA3B1),
             ),
             child: 
               SvgPicture.asset('assets/icons/arrow-left.svg',
-              height: 25,
-              width: 25,
+              height: 25.5,
+              width: 25.5,
             ),
+          ),
         ),
-        ),
-
         actions: [
           GestureDetector(
             onTap: () {
               Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PomodoroSettings()),
-            );
+                context,
+                MaterialPageRoute(builder: (context) => const PomodoroSettings()),
+              );
             },
             child: Container(
               margin: const EdgeInsets.all(10),
               alignment: Alignment.center,
               width: 37,
               decoration: const BoxDecoration(
-                color: Color(0xFF6493A5),
+                color: Color(0xFF0FA3B1),
               ),
               child: 
                 SvgPicture.asset('assets/icons/settings-2.svg',
-                height: 25,
-                width: 25,
+                height: 25.5,
+                width: 25.5,
               ),
             ),
           )
-          
         ],
       ),
 
-      body: Center(
-        child: CircularCountDownTimer(
-          width: MediaQuery.of(context).size.width / 2.5,
-          height: MediaQuery.of(context).size.height / 2.5,
-          duration: 150,
-          fillColor: const Color(0xFFF9F7F3),
-          ringColor: Colors.blue,
-          controller: _controller,
-          backgroundColor: Colors.white,
-          strokeWidth: 15.0,
-          strokeCap: StrokeCap.round,
-          isTimerTextShown: true,
-          isReverseAnimation: true,
-          isReverse: true,
-          autoStart: true,
-          textFormat: CountdownTextFormat.MM_SS,
-          textStyle: const TextStyle(
-            fontFamily: 'Poppins',
-            fontSize: 50.0,
-            color: Colors.black),
+      body: Container(
+        padding: const EdgeInsets.only(bottom: 200.0),
+        margin: const EdgeInsets.all(50.0),
+        //color: Colors.black,
+        child: Center(
+          child: CircularCountDownTimer(
+            width: MediaQuery.of(context).size.width / 2.5,
+            height: MediaQuery.of(context).size.height / 2.5,
+            duration: 150,
+            fillColor: const Color(0xFF0FA3B1),
+            ringColor: const Color(0xFFF9F7F3),
+            controller: _controller,
+            backgroundColor: const Color(0xFFF9F7F3),
+            strokeWidth: 15.0,
+            strokeCap: StrokeCap.round,
+            isTimerTextShown: true,
+            isReverseAnimation: true,
+            isReverse: true,
+            autoStart: true,
+            textFormat: CountdownTextFormat.MM_SS,
+            textStyle: const TextStyle(
+              fontFamily: 'Poppins',
+              fontSize: 50.0,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFF333232)),
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        backgroundColor: const Color.fromARGB(255, 100, 147, 165),
-        onPressed: (){
-          setState(() {
-            if(_isPause){
-              _isPause = false;
-              _controller.resume();
-            }
-            else{
-              _isPause = true;
-              _controller.pause();
-            }
-          });
-        }, 
-        icon: Icon(_isPause ? Icons.play_arrow : Icons.pause),
-        label: Text(_isPause ? 'Resume' : 'Pause' ),
-        
+      
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(bottom: 70.0),
+        child: FloatingActionButton.extended(
+          backgroundColor: const Color(0xFF0FA3B1),
+          onPressed: (){
+            setState(() {
+              if(_isPause){
+                _isPause = false;
+                _controller.resume();
+              }
+              else{
+                _isPause = true;
+                _controller.pause();
+              }
+            });
+          }, 
+          icon: Icon(_isPause ? Icons.play_arrow : Icons.pause),
+          label: Text(_isPause ? 'Resume' : 'Pause' ),
+          
+        ),
       ),
+      
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
