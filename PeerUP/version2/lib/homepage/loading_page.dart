0eefd64697747fb0177a6146ae3f1256c0a6f363
Deirael <tadepra@up.edu.ph>
2023-11-07 12:32:37 +0800

    edited loading_page

--- PeerUP/version2/lib/homepage/loading_page.dart
+++ PeerUP/version2/lib/homepage/loading_page.dart
@@ -29,32 +29,47 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromRGBO(100, 147, 165, 100),
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
             colors: [
-              Color.fromRGBO(20, 108, 148, 100), // Top color
-              Color.fromRGBO(20, 108, 148, 69.27), // Middle color
-              Color.fromRGBO(20, 108, 148, 27), // Bottom color
+              Color.fromRGBO(28, 104, 157, 69.27), // Top color
+              Color.fromRGBO(100, 147, 165, 100), // Middle color
+              Color.fromRGBO(255, 255, 255, 100), // Bottom color
             ],
           ),
         ),
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text(
-                'PEERUP',
-                style: TextStyle(
-                  fontFamily: 'Poppins',
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 45,
-                  fontWeight: FontWeight.bold,
+              children: <Widget>[
+                // Stroked text as border.
+                Text(
+                  'PEERUP',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                    foreground: Paint()
+                      ..style = PaintingStyle.stroke
+                      ..strokeWidth = 1
+                      ..color = const Color(0x9C6493A5),
+                  ),
                 ),
-              ),
-              const SizedBox(height: 100),
+                // Solid text as fill.
+                const Text(
+                  'PEERUP',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 48,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              const SizedBox(height: 45),
               ClipRRect(
                 borderRadius: BorderRadius.circular(8),
                 child: Image.asset(
