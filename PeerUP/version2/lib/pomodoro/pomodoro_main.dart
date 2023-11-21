49c99a79158f5113ed73699045ac7ddde97b3f46
Kyle <98733847+Rojane-Kyle@users.noreply.github.com>
2023-11-21 14:23:47 +0800

    Edited pomodoro timer

--- PeerUP/version2/lib/pomodoro/pomodoro_main.dart
+++ PeerUP/version2/lib/pomodoro/pomodoro_main.dart
@@ -4,29 +4,62 @@ import 'package:flutter/material.dart';
 import 'package:peerup/homepage/technique.dart';
 import 'package:peerup/pomodoro/pomodoro_settings.dart';
 
+
 class Pomodoro extends StatefulWidget {
   const Pomodoro({super.key});
   @override
   State<Pomodoro> createState() => _PomodoroState();
 }
 
+class CountDownTimer {
+  //final int duration;
+  //final Color fillColor;
+  final Function onComplete;
+
+  CountDownTimer({
+    //required this.duration,
+    //@required this.fillColor,
+    required this.onComplete,
+  });
+}
+
+
 class _PomodoroState extends State<Pomodoro> {
 
   final CountDownController _controller = CountDownController();
   bool _isStarted = false;
-  int _focusTimer = 150;
-  int _shortBreak = 30;
-  int _longBreak = 60;
-  
+  bool _isOnging = false;
+  bool _focus = true;
+  bool _short = false;
+  int _focusTimer = 10;
+  final int _shortBreak = 5;
+  final int _longBreak = 7;
+  final int _session = 4;
+  int _timerDuration = 10;
 
   @override
   Widget build(BuildContext context) {
-    
+
+    // CountDownTimer _countDownTimer = CountDownTimer(
+    //   onComplete: (){
+    //     setState(() async {
+    //       await NDialog(
+    //         dialogStyle: DialogStyle(titleDivider: true),
+    //         title: Text("Timer Completed"),
+    //         //content: Text("Time to break."),
+            
+    //       ).show(context);
+          
+          
+    //     });
+    //   }, 
+    // );
+
     return Scaffold(
       backgroundColor: const Color(0xFFF9F7F3),
+      
       // Header of Pomodoro Page
       appBar: AppBar(
-        
         toolbarHeight: 70.0,
         title: const Text(
           'POMODORO',
@@ -40,49 +73,42 @@ class _PomodoroState extends State<Pomodoro> {
         backgroundColor: const Color(0xFFF9F7F3),
         centerTitle: true,
         elevation: 1.5,
-        // Arrow back icon
+        
+        // Arrow back icon in pomodoro header
         leading: GestureDetector(
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => const Techniques()),
-            );
-          },
+          );},
           child: Container(
             margin: const EdgeInsets.all(15),
             alignment: Alignment.center,
-            decoration: const BoxDecoration(
-              color: Color(0xFFF9F7F3),
-            ),
+            decoration: const BoxDecoration(color: Color(0xFFF9F7F3)),
             child:  const Icon(Icons.arrow_back_rounded, color: Color(0xFF333232), size: 32.0,)
           ),
         ),
-        // Pomodoro settings icon
+        
+        // Pomodoro settings icon in pomodoro header
         actions: [
           GestureDetector(
             onTap: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const PomodoroSettings()),
-              );
-            },
+            );},
             child: Container(
-              margin: const EdgeInsets.all(15),
-              alignment: Alignment.center,
               width: 37,
-              decoration: const BoxDecoration(
-                color: Color(0xFFF9F7F3),
-                
-              ),
-              child: 
-                SvgPicture.asset('assets/icons/settings-2.svg',
-                height: 27.0,
-                width: 27.0,
-              ),
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(15),
+              decoration: const BoxDecoration(color: Color(0xFFF9F7F3),),
+              child: SvgPicture.asset('assets/icons/settings-2.svg', height: 27.0, width: 27.0,),
             ),
           )
         ],
       ),
+
+      // Circular countdown timer
       body: Container(
         padding: const EdgeInsets.only(bottom: 200.0),
         margin: const EdgeInsets.all(50.0),
@@ -103,12 +129,53 @@ class _PomodoroState extends State<Pomodoro> {
             isReverse: true,
             autoStart: false,
             textFormat: CountdownTextFormat.MM_SS,
-            //onComplete: () => ,
             textStyle: const TextStyle(
               fontFamily: 'Poppins',
               fontSize: 50.0,
               fontWeight: FontWeight.bold,
               color: Color(0xFF333232)),
+            onComplete: () {
+              if (_isOnging){
+                showDialog(
+                context: context,
+                builder: (BuildContext dialogContext) {
+                  return const Dialog(
+                    //insetAnimationDuration: const Duration(seconds: 3.0),
+                    
+                    insetPadding: EdgeInsets.all(20.0),
+                    backgroundColor: Color(0xFFF9F7F3),
+                    shadowColor: Color(0xFF333232),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text("Congrats!",
+                        style: TextStyle( fontFamily: 'Poppins', fontSize: 15.0),
+                      ),
+                    ),
+                  );
+                },
+              );
+              }
+              // _focusTimer = _shortBreak;
+              // _controller.reset();
+              // _controller.start();
+            },
+            onStart: () {
+              debugPrint('Countdown Started');
+            },
+            // onChange: (String timeStamp){
+            //   // if ($timeStamp = ''){
+
+            //   // }
+            //   debugPrint('Countdown Changed $timeStamp');
+            // },
+            // onComplete: () {
+            //   Container(
+            //     margin: EdgeInsets.only(left: 0.0,top: 60.0, bottom: 50.0, right:0.0),    
+            //     child: Text('denifniefre', style: new 
+            //       TextStyle( color: Colors.black, fontWeight: FontWeight.bold, fontSize: 100.0 ))
+            //   , );
+            //   //debugPrint('Countdown Changed $timeStamp');
+            // }
           ),
         ),
       ),
@@ -123,9 +190,11 @@ class _PomodoroState extends State<Pomodoro> {
             setState(() {
               if(_isStarted == false){
                 _isStarted = true;
-                _controller.restart();
-              }else if (_isStarted == true){
+                _isOnging = true;
+                _controller.start();
+              }else if (_isStarted == true && _isOnging == true){
                 _isStarted = false;
+                _isOnging = false;
                 _controller.reset();
             }});
           }, 
@@ -146,3 +215,5 @@ class _PomodoroState extends State<Pomodoro> {
     );
   }
 }
+
+
