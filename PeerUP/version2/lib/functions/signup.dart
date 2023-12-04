5e4e424b932c8acdf8ff79aa5a414c0187445d04
jarenmatthew <99801133+jarenmatthew@users.noreply.github.com>
2023-12-05 02:21:43 +0800

    added functions folder for FIREBASE SignUp call

---
+++ PeerUP/version2/lib/functions/signup.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future<UserCredential?> signUp(String email, String password) async {
+  try {
+    UserCredential userCredential =
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    return userCredential;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+    return null;
+  } catch (e) {
+    print(e);
+    return null;
+  }
+}
