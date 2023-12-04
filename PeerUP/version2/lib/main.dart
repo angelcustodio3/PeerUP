2831c9f51c2bca92ca9178217d0ee91ca0ed40e1
jarenmatthew <99801133+jarenmatthew@users.noreply.github.com>
2023-12-05 02:23:17 +0800

    Update main.dart (added firebase connection)

--- PeerUP/version2/lib/main.dart
+++ PeerUP/version2/lib/main.dart
@@ -1,11 +1,20 @@
 // ignore_for_file: unused_import
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:peerup/firebase_options.dart';
 import 'package:peerup/homepage/loading_page.dart';
 import 'package:peerup/homepage/mainpage.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
