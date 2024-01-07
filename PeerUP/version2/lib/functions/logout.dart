1393df1c0f52cc08b99f696ebbe92e40ff259811
jarenmatthew <isaerjarenmatthewong@gmail.com>
2024-01-08 02:54:47 +0800

    test commit

--- PeerUP/version2/lib/functions/logout.dart
+++ PeerUP/version2/lib/functions/logout.dart
@@ -6,6 +6,8 @@ import 'package:peerup/homepage/homepage.dart';
 
 Future<UserCredential?> logOut(context) async {
   await FirebaseAuth.instance.signOut();
+  print("testing");
+
   Navigator.of(context).pushReplacement(
     MaterialPageRoute(builder: (context) => const Login()),
   );
